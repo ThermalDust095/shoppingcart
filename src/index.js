74ee66c67b1dfec99f0ff09c8afa05247db5487d
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2023-01-24 16:23:38 +0530

    1.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './components/counter';
 import 'bootstrap/dist/css/bootstrap.css'
+import Counters from './components/counters';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Counters />
   </React.StrictMode>
 );
 
