de47f85770fe757b7bedc6b68a413d6f5b8c4a8b
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2023-01-25 11:40:17 +0530

    1.2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React,{ Component } from "react";
 import 'bootstrap/dist/css/bootstrap.css'
 class Counter extends Component { 
     state = {
-        value:this.props.value
+        value : this.props.value
     }
 
     styles = {
@@ -14,23 +14,26 @@ class Counter extends Component {
         this.setState({value: this.state.value+1})
     };
 
+    handleDecrement = () =>{
+        let value = this.state.value;
+        (value<=0) ? this.setState({value: 0}):this.setState({value:value-1});
+    }   
+
+
+
     render() { 
         let classes = "badge ";
         classes += (this.state.value === 0) ? "badge-warning m-2" : "badge-info m-2";
-
+        
         return( <div>
                 <span style={this.styles} className= {classes}>{this.state.value}</span>
                 <button 
                 onClick = {()=>this.handleIncrement()}  
-                className="btn btn-outline-info btn-sm">Increment</button>
-                <button onClick={()=>this.props.onDelete(this.props.id)} className="btn btn-outline-danger btn-sm m-2">Delete</button>
+                className="btn btn-outline-info btn-sm">+</button>
+                <button onClick={()=>this.handleDecrement()} className="btn btn-outline-danger btn-sm m-2">-</button>
+                <button onClick={()=>this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
-    }
-
-    formatCount(){
-        const { value } = this.state;
-        return value === 0 ? "Zero" : value
     }
 }
  
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -6,16 +6,40 @@ class Counters extends Component {
         counters:[
             {id:1,value:0},
             {id:2,value:0},
-            {id:3,value:0},
+            {id:3,value:4},
             {id:4,value:0}
         ]
      } 
+
+    handleDelete= counterId =>{
+        const Cntrs = this.state.counters.filter(c => c.id!== counterId)
+        this.setState({counters: Cntrs})
+    }
+
+    handleAdd=()=>{
+        let max=0;
+        console.log("To be Added");
+        this.state.counters.map(i=> (i.id>=max) ? max=i.id : i.id);
+        let cntrs=[];
+        for(let i=1;i<=max+1;i++){
+            cntrs.push({id:i,value:0})
+        }
+        console.log(cntrs)
+        this.setState({counters: cntrs})
+    }
+
     render() { 
         return (
-            <React.Fragment>
+            <div>
+                <h1>Counters</h1>
+                <button className="btn btn-secondary m-2" onClick={()=>this.handleAdd()}>+</button>
                 {this.state.counters.map(i => 
-                <Counter key={i.id} value={i.value} selected={true}/>)}
-            </React.Fragment>
+                <Counter key={i.id} 
+                onDelete = {this.handleDelete}
+                id = {i.id}
+                value={i.value} 
+                selected={true}/>)}
+            </div>
         );  
     }
 }
