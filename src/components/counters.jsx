608563274ea2394c1ef95b3cba70648500d27d21
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2023-01-26 14:05:47 +0530

    1.3

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Counter from './counter';
 import './counters.css';
+import axios from 'axios';
 
 class Counters extends Component {
     state = { 
@@ -9,17 +10,25 @@ class Counters extends Component {
             {id:2,value:0},
             {id:3,value:0},
             {id:4,value:0}
-        ]
+        ],
+        x:"Counters",
+        i:-1
      } 
 
     handleDelete= counterId =>{
         const Cntrs = this.state.counters.filter(c => c.id!== counterId)
         this.setState({counters: Cntrs})
     }
+    getData(){
+    this.state.i+=1;
+    console.log(this.state.i)
+    axios.get("https://reqres.in/api/users?page=2").then(r=> this.setState({x: r.data.data[this.state.i].email}));
+    if(this.state.i===6)this.state.i=0;
+        
+    }
 
     handleAdd=()=>{
         let max=0;
-        console.log("To be Added");
         this.state.counters.map(i=> (i.id>=max) ? max=i.id : i.id);
         let cntrs=[];
         for(let i=1;i<=max+1;i++){
@@ -32,7 +41,7 @@ class Counters extends Component {
         return (
             <div>
                 <div className='header'>
-                <h1 id="title">Counters</h1>
+                <h1 id="title">{this.state.x}</h1>
                 <button className="btn btn-secondary m-2" title="inb" onClick={()=>this.handleAdd()}>+</button>
                 </div>
                 {this.state.counters.map(i => 
@@ -41,7 +50,9 @@ class Counters extends Component {
                 id = {i.id}
                 value={i.value} 
                 selected={true}/>)}
+                <button className='btn btn-primary m-2' onClick={()=>this.getData()}>Send</button>
             </div>
+
         );  
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Counters from './components/counters';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
 root.render(
   <React.StrictMode>
     <Counters />
